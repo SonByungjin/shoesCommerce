15b451cd4eeada3b6ae0a45072639e217beadb5e
김호균 <salvation@gimhogyun-ui-MacBookPro.local>
2020-09-24 16:28:25 +0900

    [12기 김호균] 미니카트 기능 구현 완료 [3차 리뷰반영]

--- src/Routes.js
+++ src/Routes.js
@@ -5,8 +5,6 @@ import SignUp from "./Pages/SignUp/SignUp";
 import Login from "./Pages/Login/Login";
 import ProductList from "./Pages/ProductList/ProductList";
 import ProductDetail from "./Pages/ProductDetail/ProductDetail";
-import Footer from "./Components/Footer/Footer";
-import MiniCart from "./Components/MiniCart/MiniCart";
 
 class Routes extends React.Component {
   render() {
@@ -18,8 +16,6 @@ class Routes extends React.Component {
           <Route exact path="/login" component={Login} />
           <Route exact path="/productList" component={ProductList} />
           <Route exact path="/productDetail/:id" component={ProductDetail} />
-          <Route exact path="/Footer" component={Footer} />
-          <Route exact path="/minicart" component={MiniCart} />
         </Switch>
       </Router>
     );
