0231eb6466887fb2a464f1b65e157c88b9f3a431
youngkwon315 <39914288+youngkwon315@users.noreply.github.com>
2020-09-25 13:01:54 +0900

    스크롤 상단 이동 함수 추가

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ScrollToTop from "./Components/ScrollToTop";
 import Main from "./Pages/Main/Main";
 import SignUp from "./Pages/SignUp/SignUp";
 import Login from "./Pages/Login/Login";
@@ -12,6 +13,7 @@ class Routes extends React.Component {
   render() {
     return (
       <Router>
+        <ScrollToTop />
         <Switch>
           <Route exact path="/main" component={Main} />
           <Route exact path="/signup" component={SignUp} />
